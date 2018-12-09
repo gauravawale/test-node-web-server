09a9e5e48b34c6548322c1f296fe1c15af290ae2
Awale, Gaurav <Gaurav.Awale@cdk.com>
2018-12-09 15:08:59 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method}: ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs', {
+//         pageTitle: 'Maintainance Page'
+//     });
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (textInput) => {
+    return textInput.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    //res.send('Hello Express');
+
+    //res.send('<h1>Hello Express</h1>');
+
+    // res.send({
+    //     name: 'Gaurav',
+    //     likes: [
+    //         'biking',
+    //         'trekking'
+    //     ]
+    // });
+
+    res.render('homepage.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to homepage'
+    });
+});
+
+app.get('/about', (req, res) => {
+    //res.send('Hello Express on About page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        request: 'bad',
+        errorMessage: 'Unable to serve the request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
